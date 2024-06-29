6b5bc5e054ec179a42084445dc53179c09939079
Nicholas <nskins@umich.edu>
2024-06-29 17:33:59 -0400

    Shoot bullet from triangle every half a second

--- Triangle.gd
+++ Triangle.gd
@@ -1,4 +1,14 @@
 extends Area2D
 
+@export var Bullet : PackedScene = load("res://bullet.tscn")
+
 func _process(delta):
 	look_at(get_global_mouse_position())
+
+func _on_shoot_timer_timeout():
+	shoot()
+	
+func shoot():
+	var b = Bullet.instantiate()
+	owner.add_child(b)
+	b.transform = $Muzzle.global_transform
