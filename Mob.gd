0a746b7b4b6103c7139074d166ba88a6580b1059
Nicholas <nskins@umich.edu>
2024-06-30 13:51:03 -0400

    Make mobs move toward the triangle

---
+++ Mob.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+var speed = 100
+
+# TODO: it would be better to get these numbers from the
+#       current position of the Triangle instead of hardcoding.
+var destination = Vector2(577, 324)
+
+func _physics_process(delta):
+	look_at(destination)
+	
+	var normalized_destination = destination.normalized()
+	
+	position += transform.x * speed * delta
